a845978dab0a4393d5150c3ff6c1f36dd2688496
Taketo Sano <taketo1024@gmail.com>
2014-10-18 14:49:56 +0900

    add Complex.swift , ComplexPlane.swift .

---
+++ SwiftComplex/Complex.swift
@@ -0,0 +1,98 @@
+//
+//  Complex.swift
+//  SwiftComplex
+//
+//  Created by Taketo Sano on 2014/10/18.
+//  
+//
+
+import Foundation
+
+struct Complex: Equatable, IntegerLiteralConvertible, FloatLiteralConvertible {
+    let x: Double
+    let y: Double
+    
+    init(_ x: Double, _ y: Double) {
+        self.x = x
+        self.y = y
+    }
+    
+    init(_ x: Double) {
+        self.x = x
+        self.y = 0
+    }
+    
+    init(r: Double, θ: Double) {
+        self.x = r * cos(θ)
+        self.y = r * sin(θ)
+    }
+    
+    static func convertFromIntegerLiteral(value: IntegerLiteralType) -> Complex {
+        return Complex(Double(value))
+    }
+    
+    static func convertFromFloatLiteral(value: FloatLiteralType) -> Complex {
+        return Complex(value)
+    }
+}
+
+func == (z: Complex, w: Complex) -> Bool {
+    return z.x == w.x && z.y == w.y
+}
+
+func Re(a: Complex) -> Double {
+    return a.x
+}
+
+func Im(a: Complex) -> Double {
+    return a.y
+}
+
+prefix func ~(a: Complex) -> Complex {
+    return Complex(a.x, -a.y);
+}
+
+func + (z: Complex, w: Complex) -> Complex {
+    return Complex(z.x + w.x, z.y + w.y)
+}
+
+func - (z: Complex, w: Complex) -> Complex {
+    return Complex(z.x - w.x, z.y - w.y)
+}
+
+prefix func -(z: Complex) -> Complex {
+    return Complex(-z.x, -z.y);
+}
+
+func * (a: Double, z: Complex) -> Complex {
+    return Complex(a * z.x, a * z.y)
+}
+
+func * (z: Complex, w: Complex) -> Complex {
+    return Complex(z.x * w.x - z.y * w.y, z.x * w.y + z.y * w.x)
+}
+
+func / (z: Complex, w: Complex) -> Complex {
+    let w_inv = Complex(w.x / (w.x * w.x + w.y * w.y), -w.y / (w.x * w.x + w.y * w.y))
+    return z * w_inv
+}
+
+func abs(z: Complex) -> Double {
+    return sqrt(z.x * z.x + z.y * z.y)
+}
+
+func arg(z: Complex) -> Double {
+    let r = abs(z)
+    if(r == 0) {
+        return 0
+    }
+    
+    let t = acos(z.x / r)
+    return (z.y >= 0) ? t : 2 * M_PI - t
+}
+
+func exp(z: Complex) -> Complex {
+    return Complex(exp(z.x) * cos(z.y), exp(z.x) * sin(z.y))
+}
+
+let i = Complex(0, 1)
---
+++ SwiftComplex/ComplexPlane.swift
@@ -0,0 +1,65 @@
+//
+//  ComplexPlane.swift
+//  SwiftComplex
+//
+//  Created by Taketo Sano on 2014/10/18.
+//  
+//
+
+import UIKit
+
+class ComplexPlane : UIView {
+    var unit: CGFloat = 50.0
+    var scale: CGFloat = 1.0
+    var points: [String: Complex] = [:]
+    var colors: [String: UIColor] = [:]
+    
+    subscript(name: String) -> (Complex!, UIColor!) {
+        get {
+            return (points[name], colors[name])
+        }
+        set(value) {
+            points[name] = value.0
+            colors[name] = value.1
+        }
+    }
+    
+    override func drawRect(rect: CGRect) {
+        let ctx = UIGraphicsGetCurrentContext()
+        let centerX = self.bounds.width / 2
+        let centerY = self.bounds.height / 2
+        
+        // fill background
+        CGContextSetFillColorWithColor(ctx, UIColor.whiteColor().CGColor)
+        CGContextFillRect(ctx, self.bounds)
+        
+        // draw axises
+        CGContextSetLineWidth(ctx, 1)
+        CGContextSetStrokeColorWithColor(ctx, UIColor.blackColor().CGColor)
+        
+        CGContextMoveToPoint(ctx, 0, centerY)
+        CGContextAddLineToPoint(ctx, self.bounds.width, centerY)
+        CGContextStrokePath(ctx)
+        
+        CGContextMoveToPoint(ctx, centerX, 0)
+        CGContextAddLineToPoint(ctx, centerX, self.bounds.height)
+        CGContextStrokePath(ctx)
+        
+        // draw points
+        for (name, z) in points {
+            let u = unit / scale
+            let point = CGPoint(x: centerX + u * CGFloat(z.x), y: centerY - u * CGFloat(z.y))
+            let color = (colors[name] != nil) ? colors[name]! : UIColor.blackColor()
+            
+            // draw point
+            CGContextSetFillColorWithColor(ctx, color.CGColor)
+            CGContextAddArc(ctx, point.x, point.y, 3, 0, CGFloat(2 * M_PI), 0)
+            CGContextFillPath(ctx)
+            
+            // draw name
+            let text: NSString = name
+            let textPoint = CGPoint(x: point.x + 5, y: point.y)
+            text.drawAtPoint(textPoint, withAttributes:[NSForegroundColorAttributeName: color])
+        }
+    }
+}
