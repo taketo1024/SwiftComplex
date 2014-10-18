64d622edb0ed1f5e6b6be4dfe6a0f64a60ac1e9f
Taketo Sano <taketo1024@gmail.com>
2014-10-18 15:54:50 +0900

    add auto rotation.

--- SwiftComplex/DetailViewController.swift
+++ SwiftComplex/DetailViewController.swift
@@ -11,10 +11,13 @@ import UIKit
 class DetailViewController: UIViewController {
 
     @IBOutlet weak var plane: ComplexPlane!
+    @IBOutlet weak var autoButton: UIButton!
     
     var map: ((Complex) -> Complex)?
     
     private var z: Complex = 0
+    private var timer: NSTimer?
+    private var touched = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,6 +44,8 @@ class DetailViewController: UIViewController {
             z = plane.complexAtPoint(point)
             self.update()
         }
+        
+        touched = true
     }
     
     override func touchesMoved(touches: NSSet, withEvent event: UIEvent) {
@@ -49,6 +54,31 @@ class DetailViewController: UIViewController {
         
         if (plane.pointInside(point, withEvent: nil)) {
             z = plane.complexAtPoint(point)
+            self.update()
+        }
+    }
+    
+    override func touchesCancelled(touches: NSSet!, withEvent event: UIEvent!) {
+        touched = false
+    }
+    
+    override func touchesEnded(touches: NSSet, withEvent event: UIEvent) {
+        touched = false
+    }
+    
+    @IBAction func autoButtonTapped() {
+        autoButton.selected = !autoButton.selected;
+        if(autoButton.selected) {
+            timer = NSTimer.scheduledTimerWithTimeInterval(0.01, target:self, selector:"timerFired", userInfo: nil, repeats: true)
+            timer?.fire()
+        } else {
+            timer?.invalidate()
+        }
+    }
+    
+    func timerFired() {
+        if(!touched) {
+            z = Complex(r: abs(z), θ: arg(z) + M_PI / 100)
             self.update()
         }
     }
